01fc561c89f251490e883468c2b8b5318cc27fd4
Alexander Kustenkov@ <stevehh251@gmail.com>
2024-03-28 18:44:41 +0300

    Add config

---
+++ WebJava/src/main/java/cmc/WebJava/relations/Project.java
@@ -0,0 +1,37 @@
+package cmc.WebJava.relations;
+
+import lombok.*;
+
+import jakarta.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "projects")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor(force = true)
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class Project implements DefaultEntity<Long> {
+
+    @Id
+    @Column(nullable = false, name = "proj_id")
+    private Long id;
+
+    @Column(nullable = false, name = "name")
+    @NonNull
+    private String name;
+
+    @Column(nullable = false, name = "start_date")
+    @NonNull
+    private Date start_date;
+
+    @Column(nullable = false, name = "end_date")
+    @NonNull
+    private Date end_date;
+
+    @Column(nullable = false, name = "status")
+    @NonNull
+    private String status;
+}
